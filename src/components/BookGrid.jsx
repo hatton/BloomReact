f46b2e317779775c14e63884a514e96a17c3505f
John Hatton <hattonjohn@gmail.com>
2015-12-23 16:30:18 -0700

    Move selection logic up to grid so that BookThumbnail can be dumb

--- src/components/BookGrid.jsx
+++ src/components/BookGrid.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import {actions as bookActions} from 'redux/modules/currentBook';
+
 import BookThumbnail from 'components/BookThumbnail.jsx';
-import John from 'axios';
+import Axios from 'axios';
 import './BookGrid.less';
 
 export default class BookGrid extends Component {
+
+  static propTypes = {
+    currentBook: React.PropTypes.string.isRequired,
+    selectBook: React.PropTypes.func.isRequired
+  }
+
   constructor(){
     super();
       //this.onChange = this.onChange.bind(this); //https://github.com/goatslacker/alt/issues/283
-      //start with no books, then we'll crate them when we hear back from the server
-      if(typeof state == "undefined")
-      {
-        this.state = {items:[]};
-      }
+    //start with no books, then we'll create them when we hear back from the server
+    this.state = {items:[]};
   }
   componentDidMount() {
     var self = this;
-    John.get(this.props.query).then(result => {
+    Axios.get(this.props.query).then(result => {
       var books = result.data;
       self.setState({
         items: books
@@ -23,11 +29,27 @@ export default class BookGrid extends Component {
     });
   }
   render() {
+    let selectedBookId = this.props.currentBook;
+    let selectBook = this.props.selectBook;
     var createItem = function(item) {
+      let thisIsTheThumbNailOfTheCurrentBook = item.id == selectedBookId;
       //alert('ok, now breakpoints should work'); // without this, breakpoints don't work. Used this to break into componentDidMount().
       //todo: use a unique key instead of the caption
-      return <BookThumbnail key={item.id}>{item}</BookThumbnail>;
+      return <BookThumbnail key={item.id} selected={thisIsTheThumbNailOfTheCurrentBook} onClick={() => selectBook(item.id)}>{item}</BookThumbnail>;
     };
     return     <ul className='bookGrid'>{this.state.items.map(createItem)}</ul>;
   }
 }
+
+// Which props do we want to inject, given the global state?
+// Note: use https://github.com/faassen/reselect for better performance.
+// mapStateToProps
+function selectWhatWeNeedFromGlobalState(state) {
+  return {
+    currentBook: state.currentBook,
+    selectBook: state.selectBook
+  }
+}
+// // Redux function that wraps the component to inject redux dispatch and state into it
+//connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
+export default connect(selectWhatWeNeedFromGlobalState, bookActions)(BookGrid)
--- src/components/BookThumbnail.jsx
+++ src/components/BookThumbnail.jsx
@@ -1,49 +1,24 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
 import './BookThumbnail.less';
 import classNames from 'classnames';//https://github.com/JedWatson/classnames
-import {actions as bookActions} from 'redux/modules/currentBook';
 
-class BookThumbnail extends Component {
+
+export default class BookThumbnail extends Component {
 
   static propTypes = {
-    currentBook: React.PropTypes.string.isRequired,
-    selectBook: React.PropTypes.func.isRequired
+    onClick: React.PropTypes.func.isRequired
   }
   render () {
     var thumbnailClasses = classNames(
-      'bookThumbnail'
-      ,{'selected' : this.props.currentBook == this.props.children.id}
+      'bookThumbnail',
+      {'selected' : this.props.selected}
     );
-    //{type:'SELECT_BOOK', currentBook: this.props.children.id})}>
-//              <li className={thumbnailClasses} onClick={() => this.props.dispatch({type:"SELECT_BOOK", bookId:this.props.children.id})}>
+
     return (
-              <li className={thumbnailClasses} onClick={() => this.props.selectBook(this.props.children.id)}>
+              <li className={thumbnailClasses} onClick={this.props.onClick}>
                 <img></img>
                 <h3>{this.props.children.title}</h3>
               </li>
     );
   }
 }
-
-// Which props do we want to inject, given the global state?
-// Note: use https://github.com/faassen/reselect for better performance.
-// mapStateToProps
-function selectWhatWeNeedFromGlobalState(state) {
-  return {
-    currentBook: state.currentBook
-    ,selectBook: state.selectBook
-  }
-}
-// // Redux function that wraps the component to inject redux dispatch and state into it
-//connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
-export default connect(selectWhatWeNeedFromGlobalState, bookActions)(BookThumbnail)
-
-/* this also worked:
-const mapDispatchToProps = (dispatch) => {
-  return{
-    selectBook: (id) => dispatch({type:"SELECT_BOOK", bookId:id})
-  }
-}
-export default connect(selectWhatWeNeedFromGlobalState, mapDispatchToProps)(BookThumbnail)
-*/
